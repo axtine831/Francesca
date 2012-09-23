e7be890548fe50bc5b0f95e6e80aba57dbfa1392
axtine831 <ampstine@gmail.com>
2012-09-23 17:16:47 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Francesca::Application.config.secret_token = 'ac02af44c8992fb9fa99bc3c29810e254461f4d04db484ce5d9c46bb3931829185179b25084060f207bcc3ae7491f78a64055863c8e01339dc4827702356af27'
